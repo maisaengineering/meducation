36f13c7aedaf72ddb0498a08ec4623b7deb345b8
imaginationcoder <labs@maisasolutions.com>
2015-04-07 15:59:33 +0530

    brantree details to env

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,11 +1,4 @@
-#if Rails.env.production?
-#  Braintree::Configuration.environment = :production
-#  Braintree::Configuration.merchant_id = "yqgc5nynxdxht4xg"
-#  Braintree::Configuration.public_key = "g8fh6dmdt9vnhkk4"
-#  Braintree::Configuration.private_key = "b8dbb2b195f6aac8cb808c2a3638aec5"
-#else
-  Braintree::Configuration.environment = :sandbox
-  Braintree::Configuration.merchant_id = "qyf39jcdmjx5gntz"
-  Braintree::Configuration.public_key = "gth2q43tpj57cfxx"
-  Braintree::Configuration.private_key = "9e3d675f6e491b4d225187212027f490"
-#end
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = ENV['BT_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BT_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BT_PRIVATE_KEY']
